4a8675defea6cc8f8e89c67e85ae684473359c94
SkyJedi <skyjedi@gmail.com>
2017-05-30 22:04:21 -0500

    1.2.0   -Initiative manual tracker up and working!

--- src/Initiative.js
+++ src/Initiative.js
@@ -11,8 +11,10 @@ class Initiative extends Component {
       slideout: 'none',
       message: {},
       messageRef: firebase.database().ref().child(`${channel}`).child('message'),
-      InitiativeRef: firebase.database().ref().child(`${channel}`).child('Initiative'),
+      InitiativeRef: firebase.database().ref().child(`${channel}`).child('Initiative').child('order'),
       Initiative: {},
+      InitiativePastRef: firebase.database().ref().child(`${channel}`).child('Initiative').child('past'),
+      InitiativePast: {},
       position: {},
       positionRef: firebase.database().ref().child(`${channel}`).child('Initiative').child('position'),
     };
@@ -43,6 +45,18 @@ class Initiative extends Component {
       }
     });
 
+    this.state.InitiativePastRef.on('value', snap => {
+      if (snap.val() != null) {
+      this.setState({
+        InitiativePast: snap.val()
+        });
+      } else {
+        this.setState({
+          InitiativePast: 0
+        });
+      }
+    });
+
     this.state.positionRef.on('value', snap => {
       if (snap.val() != null) {
       this.setState({
@@ -51,8 +65,8 @@ class Initiative extends Component {
       } else {
         this.setState({
           position: {
-            Round: 1,
-            Turn: 1
+            round: 1,
+            turn: 1
           }
         });
       }
@@ -70,11 +84,63 @@ slideOut() {
 InitiativeAdd() {
   this.state.InitiativeRef.push().set('PC');
 }
+
 InitiativeRemove() {
   if (this.state.Initiative !== 0) {
     this.state.InitiativeRef.child(Object.keys(this.state.Initiative)[Object.keys(this.state.Initiative).length-1]).remove();
   }
 }
+
+InitiativePrevious() {
+  let position = Object.assign({}, this.state.position);
+  let Initiative = Object.assign({}, this.state.Initiative);
+  let InitiativePast = Object.assign({}, this.state.InitiativePast);
+  if (position.turn === 1 && position.round === 1) {
+    return;
+  }
+  if (this.total() === 0) {
+    position.turn = 1;
+    position.round = 1;
+  } else if (position.turn - 1 < 1) {
+    position.turn = this.total();
+    position.round--;
+    InitiativePast = Initiative;
+    Initiative = {};
+    Initiative[Object.keys(InitiativePast)[Object.keys(InitiativePast).length-1]] = Object.values(InitiativePast)[Object.values(InitiativePast).length-1];
+    delete InitiativePast[Object.keys(InitiativePast)[Object.keys(InitiativePast).length-1]];
+  } else {
+    position.turn--;
+    Initiative[Object.keys(InitiativePast)[Object.keys(InitiativePast).length-1]] = Object.values(InitiativePast)[Object.values(InitiativePast).length-1];
+    delete InitiativePast[Object.keys(InitiativePast)[Object.keys(InitiativePast).length-1]];
+  }
+  this.state.positionRef.set(position);
+  this.state.InitiativeRef.set(Initiative);
+  this.state.InitiativePastRef.set(InitiativePast);
+}
+
+InitiativeNext() {
+  if (this.total() === 0) {
+    return;
+  }
+  let position = Object.assign({}, this.state.position);
+  let Initiative = Object.assign({}, this.state.Initiative);
+  let InitiativePast = Object.assign({}, this.state.InitiativePast);
+  if (position.turn >= this.total()) {
+    position.turn = 1;
+    position.round++;
+    InitiativePast[Object.keys(Initiative)[0]] = Object.values(Initiative)[0];
+    Initiative = InitiativePast;
+    InitiativePast = 0;
+  } else {
+    position.turn++;
+    InitiativePast[Object.keys(Initiative)[0]] = Object.values(Initiative)[0];
+    delete Initiative[Object.keys(Initiative)[0]];
+  }
+  this.state.positionRef.set(position);
+  this.state.InitiativeRef.set(Initiative);
+  this.state.InitiativePastRef.set(InitiativePast);
+}
+
 flip (v, k) {
   if (v === 'PC') {
     this.state.InitiativeRef.child(k).set('NPC');
@@ -83,6 +149,17 @@ flip (v, k) {
   }
 }
 
+flipPast (v, k) {
+  if (v === 'PC') {
+    this.state.InitiativePastRef.child(k).set('NPC');
+  } else {
+    this.state.InitiativePastRef.child(k).set('PC');
+  }
+}
+
+total() {
+  return (Object.keys(this.state.Initiative).length + Object.keys(this.state.InitiativePast).length);
+}
 
   render() {
     return (
@@ -90,11 +167,10 @@ flip (v, k) {
         <div className='destiny-box' style={{display: this.state.slideout}}>
           <div style={{float: 'left', marginLeft: 6}}>
             <button onClick={this.InitiativeAdd.bind(this)} className='btnAdd' style={{display: 'inline-block'}}>+</button>
-            <button className='btnAdd' style={{display: 'inline-block'}}>←</button>
+            <button onClick={this.InitiativeNext.bind(this)}className='btnAdd' style={{display: 'inline-block'}}>→</button>
             <br/>
             <button onClick={this.InitiativeRemove.bind(this)} className='btnAdd' style={{display: 'inline-block'}}>-</button>
-            <button className='btnAdd' style={{display: 'inline-block'}}>→</button>
-
+            <button onClick={this.InitiativePrevious.bind(this)}className='btnAdd' style={{display: 'inline-block'}}>←</button>
           </div>
           <div style={{marginLeft: '45px'}}>
             {Object.entries(this.state.Initiative).map(([k,v])=>
@@ -107,10 +183,21 @@ flip (v, k) {
                 className='tokens' />
               </span>
             )}
+            <img src={`/images/repeat.png`} alt='' className='tokens' />
+            {Object.entries(this.state.InitiativePast).map(([k,v])=>
+              <span
+              key={k}
+              onClick={this.flipPast.bind(this, v, k)}>
+              <img
+                src={`/images/${v}.png`}
+                alt={v}
+                className='tokens' />
+              </span>
+            )}
           </div>
         </div>
         <button type="button" style={{marginBottom: '0.5em'}}onClick={this.slideOut.bind(this)} className='lrgButton'>Show Initiative</button>
-        <span>Round: {this.state.position.Round}<nsbr/> Turn: {this.state.position.Turn}</span>
+        <span>Round: {this.state.position.round}<nsbr/> Turn: {this.state.position.turn}</span>
       </div>
     );
   }
