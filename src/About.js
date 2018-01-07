f898993c03c810ca5f271c16c93ecc57bcc590b6
SkyJedi <skyjedi@gmail.com>
2018-01-06 20:28:51 -0600

    stuff

--- src/About.js
+++ src/About.js
@@ -4,9 +4,9 @@ const About = () => {
     return (
       <div>
         <p>Created by SkyJedi</p>
-        <p>Questions? Comments? <a href="mailto:skyjedi@gmail.com?subject=D1-C3%20Feedback" target="_blank">Contact Me</a></p>
-        <p><a href="https://paypal.me/SkyJedi" target="_blank">Donate</a></p>
-        <h6>An assistant (dice roller, chat, destiny tracker, character tracker, initiative tracker) for <a href='http://fantasyflightgames.com/' target="_blank">Fantasy Flight Games</a>, Star Wars: Edge of the Empire, Age of Rebellion, and Force and Destiny</h6>
+        <p>Questions? Comments? <a href="mailto:skyjedi@gmail.com?subject=D1-C3%20Feedback" target="_blank" rel="noopener noreferrer">Contact Me</a></p>
+        <p>An assistant (dice roller, chat, Story Point tracker, character tracker, initiative tracker) for <a href='http://fantasyflightgames.com/' target="_blank" rel="noopener noreferrer">Fantasy Flight Games</a>, <a href='https://www.fantasyflightgames.com/en/products/genesys/' target="_blank">Genesys</a></p>
+        <p><a href="https://paypal.me/SkyJedi" target="_blank" rel="noopener noreferrer">Donate</a></p>
       </div>
     )
 }
